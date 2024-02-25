d047e44b01aa58e9e87e17612093a472a25c81f9
Anshul Dalal <anshulusr@gmail.com>
2024-02-25 18:48:59 +0530

    analyzer: return the identifier along with semantic error
    Improves diagnostics by show the user where the erroring ident
    lies in the source program.

--- src/analyzer.rs
+++ src/analyzer.rs
@@ -1,7 +1,21 @@
 use crate::parser::{DirectValue, Expression, Ident, Program, Scope, Stmts};
 
 #[derive(Debug)]
-pub enum SemanticError {
+pub struct SemanticError {
+	kind: SemanticErrorKind,
+	identifier: Ident,
+}
+impl SemanticError {
+	fn new(kind: SemanticErrorKind, ident: Ident) -> Self {
+		Self {
+			kind,
+			identifier: ident,
+		}
+	}
+}
+
+#[derive(Debug)]
+pub enum SemanticErrorKind {
 	UseBeforeDeclaration,
 	MultipleDeclaration,
 }
@@ -13,11 +27,9 @@ pub fn analyze(program: Program) -> Result<(), SemanticError> {
 	} = program;
 	let mut stack = ScopeStack::new();
 	stack.scope_analyze(global_scope)?;
-	if stack.expression_valid(return_value) {
-		Ok(())
-	} else {
-		Err(SemanticError::UseBeforeDeclaration)
-	}
+	stack
+		.expression_valid(return_value)
+		.map_err(|ident| SemanticError::new(SemanticErrorKind::UseBeforeDeclaration, ident))
 }
 
 type ScopeTable = Vec<String>;
@@ -28,47 +40,57 @@ impl ScopeStack {
 	fn new() -> Self {
 		Self(Vec::new())
 	}
-	fn find_ident(&self, ident: &Ident) -> bool {
-		self.0
+	fn find_ident(&self, ident: &Ident) -> Result<(), Ident> {
+		if self
+			.0
 			.iter()
 			.flatten()
 			.find(|i| **i == ident.name)
 			.is_some()
+		{
+			Ok(())
+		} else {
+			// TODO: Make ident implement Copy to remove this clone
+			Err(ident.clone())
+		}
 	}
-	fn expression_valid(&self, expr: Expression) -> bool {
-		let find_direct_value = |direct_value| -> bool {
+	fn expression_valid(&self, expr: Expression) -> Result<(), Ident> {
+		let find_direct_value = |direct_value| -> Result<(), Ident> {
 			match direct_value {
 				DirectValue::Ident(i) => self.find_ident(&i),
-				DirectValue::Const(_) => true,
+				DirectValue::Const(_) => Ok(()),
 			}
 		};
 		match expr {
 			Expression::DirectValue(d_value) => find_direct_value(d_value),
 			Expression::BinaryExpression(l_value, _, r_value) => {
-				find_direct_value(l_value) && find_direct_value(r_value)
+				find_direct_value(l_value).and_then(|_| find_direct_value(r_value))
 			}
 		}
 	}
 	fn scope_analyze(&mut self, scope: Scope) -> Result<(), SemanticError> {
 		self.0.push(ScopeTable::new());
 		for stmt in scope.statements {
+			use SemanticErrorKind::*;
 			match stmt {
-				Stmts::Decl(Ident { name, .. }) => {
+				Stmts::Decl(ident) => {
 					let current_table = self.0.last_mut().unwrap();
-					if current_table.contains(&name) {
-						return Err(SemanticError::MultipleDeclaration);
+					if current_table.contains(&ident.name) {
+						return Err(SemanticError::new(MultipleDeclaration, ident));
 					}
-					current_table.push(name)
+					current_table.push(ident.name)
 				}
 				Stmts::Assignment(ident, expr) => {
-					if !(self.find_ident(&ident) && self.expression_valid(expr)) {
-						return Err(SemanticError::UseBeforeDeclaration);
+					if let Err(ident) = self
+						.find_ident(&ident)
+						.and_then(|_| self.expression_valid(expr))
+					{
+						return Err(SemanticError::new(UseBeforeDeclaration, ident));
 					}
 				}
 				Stmts::If(expr, scope) => {
-					if !self.expression_valid(expr) {
-						return Err(SemanticError::UseBeforeDeclaration);
-					};
+					self.expression_valid(expr)
+						.map_err(|ident| SemanticError::new(UseBeforeDeclaration, ident))?;
 					self.scope_analyze(scope)?
 				}
 			}
